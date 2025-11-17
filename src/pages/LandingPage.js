384e87e35c30fa8e952ef370c47c28bf05e47914
AuroraCoders <shehnazhaider3@gmail.com>
2025-11-17 12:30:14 +0500

    Chnage jenkin file

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -6,7 +6,7 @@ function LandingPage() {
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
       <h1>Welcome to Jenkins Project Landing Page</h1>
-      <p>This is the landing page.</p>
+      <p>This is the landing page Hello!.</p>
       <Link to="/login">
         <button>Login</button>
       </Link>
